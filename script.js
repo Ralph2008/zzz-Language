0f5ba097b8ac6067d9b4f36bbcda897be20186de
Ralph2008 <ghalysekkat@gmail.com>
2022-01-29 21:51:34 +0100

    Functions' creation & start of the dictionary
    Création de la fonction split et début du dictionnaire.
    Création de la page HTML ainsi que de la page de style.

---
+++ script.js
@@ -0,0 +1,35 @@
+function splitSentence(sentence){
+    return (sentence.split(' '));
+}
+
+function traduceWord(word){
+    switch (word) {
+        case "zZ":
+          return "oui";
+          break;
+        case "Zz":
+          return "non";
+          break;
+        case "Zz":
+          return "non";
+          break;
+        case "zzz":
+            return "bonjour";
+            break;
+        case "ZZZ":
+            return "au revoir";
+            break;
+        case "ZZ":
+            return "s'il te plaît";
+            break;
+        case "z":
+            return "de rien";
+            break;
+        case "zzzz":
+            return "Merci";
+            break;
+        default:
+          return "Sorry, a word of your sentence has no translation in zzz-language for the moment.";
+      }      
+}
+
