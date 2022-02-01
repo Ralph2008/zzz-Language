da963ede5be128070cd93ed575e2777fa0355f66
Ralph2008 <ghalysekkat@gmail.com>
2022-02-01 22:41:05 +0100

    Indentations
    Indentations du script arrangées

--- script.js
+++ script.js
@@ -6,46 +6,46 @@ function splitSentence(sentence){
 
 function traduceWord(word){
     switch (word) {
-        case "zZ":
-          return "oui";
-          break;
-        case "Zz":
-          return "non";
-          break;
-        case "Zz":
-          return "non";
-          break;
-        case "zzz":
-           return "bonjour";
-           break;
-        case "ZZZ":
-           return "au revoir";
-           break;
-        case "ZZ":
-           return "s'il te plaît";
-           break;
-        case "zzzz":
-           return "merci";
-           break;
-        case "z":
-           return "je";
-           break;
-        case "zZz":
-           return "an";
-           break;
-        case "ZZz":
-           return "aime(r)";
-           break;
-        case "Z":
-           return "abeille(s)";
-           break;
-        case "zzzzz":
-           return "s'appele(r)";
-           break;
-	case "zzZzz":
-	   return "shogi";
-	   break;
-        default:
+         case "zZ":
+            return "oui";
+            break;
+         case "Zz":
+            return "non";
+            break;
+         case "Zz":
+            return "non";
+            break;
+         case "zzz":
+            return "bonjour";
+            break;
+         case "ZZZ":
+            return "au revoir";
+            break;
+         case "ZZ":
+            return "s'il te plaît";
+            break;
+         case "zzzz":
+            return "merci";
+            break;
+         case "z":
+            return "je";
+            break;
+         case "zZz":
+            return "an";
+            break;
+         case "ZZz":
+            return "aime(r)";
+            break;
+         case "Z":
+            return "abeille(s)";
+            break;
+         case "zzzzz":
+            return "s'appele(r)";
+            break;
+	      case "zzZzz":
+	         return "shogi";
+	         break;
+         default:
           return "Sorry, a word of your sentence has no translation in zzz-language for the moment.";
       }      
 }
